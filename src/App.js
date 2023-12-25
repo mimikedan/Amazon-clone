48560070a51eed5c335a6f0e3831273cb9b2c99c
mimikedan <147217471+mimikedan@users.noreply.github.com>
2023-12-24 21:06:31 -0500

    gh pages

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ function App() {
   }, []);
 
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <div className="App">
         <Routes>
           <Route path="/login" element={<Login />} />
